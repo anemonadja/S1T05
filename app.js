88cc4fc0a8d0b2fcef15acfae79097a07e6fd717
anemonadja <nadja.villarroel.n@gmail.com>
2025-06-15 23:42:43 -0400

    se resuelve suma de notas

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const misNotas = [6, 8, 9, 2, 5, 10];
-const promedio = calcularPromedio(misNotas);
-console
+const notasFinales = [6, 8, 9, 2, 5, 10];
+
+const sumaNotas = notasFinales.reduce(
+    (sum, item) => sum + item,
+    0
+);
+
+const infoSuma = (sumaNotas) => {
+    console.log("La suma de notas es: ", sumaNotas);
+}
+
+infoSuma(sumaNotas);
